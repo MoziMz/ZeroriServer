e82eb08227d7df19b25d9ce8d9f7ed8d2a005abf
YunYeoKyeong99 <ejmacdayori2e@gmail.com>
2021-11-17 00:49:58 +0900

    fix: SessionUserArgResolver setting

--- src/main/java/com/mozi/moziserver/config/AppConfig.java
+++ src/main/java/com/mozi/moziserver/config/AppConfig.java
@@ -5,14 +5,18 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.mozi.moziserver.security.SessionUserArgResolver;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 @Configuration
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
 
     //Korea Time
     @Bean
@@ -25,5 +29,10 @@ public class AppConfig {
                 .featuresToEnable(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL)
                 .serializers(new LocalDateTimeSerializer(DateTimeFormatter.ISO_OFFSET_DATE_TIME))
                 .deserializers(new LocalDateTimeDeserializer(DateTimeFormatter.ISO_OFFSET_DATE_TIME));
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new SessionUserArgResolver());
     }
 }
