f15665534213aa2f8d950d0afac422858b77394e
JoHyoju04 <whgywn369@gmail.com>
2021-11-29 02:04:04 +0900

    feat: add annotation

--- src/main/java/com/mozi/moziserver/repository/QuestionRepository.java
+++ src/main/java/com/mozi/moziserver/repository/QuestionRepository.java
@@ -2,6 +2,8 @@ package com.mozi.moziserver.repository;
 
 import com.mozi.moziserver.model.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface QuestionRepository extends JpaRepository<Question, Long>, QuestionRepositorySupport{
 }
