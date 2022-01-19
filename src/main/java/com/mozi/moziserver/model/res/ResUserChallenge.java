73de314d6fa6195e72bd4f79f98c92b4e5b7e066
YunYeoKyeong99 <ejmacdayori2e@gmail.com>
2022-01-19 22:35:00 +0900

    fix: planDate

--- src/main/java/com/mozi/moziserver/model/res/ResUserChallenge.java
+++ src/main/java/com/mozi/moziserver/model/res/ResUserChallenge.java
@@ -1,18 +1,23 @@
 package com.mozi.moziserver.model.res;
 
+import com.mozi.moziserver.model.PlanDate;
 import com.mozi.moziserver.model.entity.UserChallenge;
 import com.mozi.moziserver.model.mappedenum.UserChallengeStateType;
 import lombok.Getter;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Getter
 public class ResUserChallenge {
     private Long seq;
     private UserChallengeStateType state;
     private LocalDate startDate;
-    private String planDate;
+    private List<ResPlanDate> planDateList;
     private Integer totalConfirmCnt;
     private ResUser user;
 //    private ResChallenge challenge;
@@ -23,7 +28,11 @@ public class ResUserChallenge {
         this.seq = userChallenge.getSeq();
         this.state = userChallenge.getState();
         this.startDate = userChallenge.getStartDate();
-        this.planDate = userChallenge.getPlanDate();
+        this.planDateList = Optional.ofNullable(userChallenge.getPlanDateList())
+                .stream()
+                .flatMap(Collection::stream)
+                .map(ResPlanDate::of)
+                .collect(Collectors.toList());
         this.totalConfirmCnt = userChallenge.getTotalConfirmCnt();
         this.user = ResUser.of(userChallenge.getUser());
 //        this.challenge = ResChallenge.of(userChallenge.getChallenge());
