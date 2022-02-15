1d7fe42bef6418897ef50be1b56392a61b592f4f
JoHyoju04 <whgywn369@naver.com>
2022-02-15 14:22:05 +0900

    feat: fix DeclarationType

--- src/main/java/com/mozi/moziserver/model/mappedenum/DeclarationType.java
+++ src/main/java/com/mozi/moziserver/model/mappedenum/DeclarationType.java
@@ -6,11 +6,11 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum DeclarationType {
-    성적인_콘텐츠("성적인 콘텐츠"),
-    혐오_발언_또는_상징("혐오 발언,상징"),
-    폭력적_콘텐츠("폭력적 콘텐츠"),
-    거짓정보("거짓정보"),
-    기타("기타");
+    SEXUAL_CONTENT("성적인 콘텐츠"),
+    HATE_SPEECH("혐오 발언,상징"),
+    VIOLENT_CONTENT("폭력적 콘텐츠"),
+    FALSE_INFORMATION("거짓정보"),
+    ETC("기타");
 
     private final String name;
 }
