3cda6bf1be5fdf3627f3a299f3e023070b54b37c
JoHyoju04 <whgywn369@gmail.com>
2022-09-14 19:35:19 +0900

    fix: Change build.gradle about ElasticSearch

--- src/main/java/com/mozi/moziserver/MoziServerApplication.java
+++ src/main/java/com/mozi/moziserver/MoziServerApplication.java
@@ -1,5 +1,6 @@
 package com.mozi.moziserver;
 
+import co.elastic.apm.attach.ElasticApmAttacher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -13,6 +14,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class MoziServerApplication {
 
     public static void main(String[] args) {
+        ElasticApmAttacher.attach();
         SpringApplication.run(MoziServerApplication.class, args);
     }
 
