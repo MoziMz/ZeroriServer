05ec0e359fd22e11812265482fe54f37565ff727
YunYeoKyeong99 <ejmacdayori2e@gmail.com>
2023-02-10 15:05:35 +0900

    [feat] #71 - Add Confirm admin API get Confirm List, update ConfirmState and delete Confirm

--- src/main/java/com/mozi/moziserver/repository/ConfirmRepositoryImpl.java
+++ src/main/java/com/mozi/moziserver/repository/ConfirmRepositoryImpl.java
@@ -1,9 +1,6 @@
 package com.mozi.moziserver.repository;
 
 import com.mozi.moziserver.model.entity.*;
-import com.mozi.moziserver.model.entity.QChallenge;
-import com.mozi.moziserver.model.entity.QConfirm;
-import com.mozi.moziserver.model.entity.QUser;
 import com.mozi.moziserver.model.mappedenum.ConfirmStateType;
 import com.querydsl.core.types.Predicate;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
@@ -11,16 +8,13 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-import java.util.*;
+import java.util.List;
+import java.util.Optional;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 
-
-public class ConfirmRepositoryImpl extends QuerydslRepositorySupport implements ConfirmRepositorySupport{
+public class ConfirmRepositoryImpl extends QuerydslRepositorySupport implements ConfirmRepositorySupport {
     private final QConfirm qConfirm = QConfirm.confirm;
     private final QUser qUser = QUser.user;
     private final QChallenge qChallenge = QChallenge.challenge;
@@ -30,12 +24,15 @@ public class ConfirmRepositoryImpl extends QuerydslRepositorySupport implements 
     private EntityManager entityManager;
 
 
-    public ConfirmRepositoryImpl() { super(Confirm.class); }
+    public ConfirmRepositoryImpl() {
+        super(Confirm.class);
+    }
 
     @Override
-    public List<Confirm> findAll(Long prevLastConfirmSeq,Integer pageSize){
+    public List<Confirm> findAll(Long prevLastConfirmSeq, Integer pageSize) {
         Predicate[] predicates = new Predicate[]{
-                predicateOptional(qConfirm.seq::lt,prevLastConfirmSeq),
+                predicateOptional(qConfirm.seq::lt, prevLastConfirmSeq),
+                qConfirm.state.notIn(ConfirmStateType.DELETED, ConfirmStateType.BLOCKED)
         };
 
         List<Confirm> confirmList = from(qConfirm)
@@ -53,8 +50,9 @@ public class ConfirmRepositoryImpl extends QuerydslRepositorySupport implements 
     @Override
     public List<Confirm> findAllByChallenge(Challenge challenge, Long prevLastConfirmSeq, Integer pageSize) {
         Predicate[] predicates = new Predicate[]{
-                predicateOptional(qConfirm.seq::lt,prevLastConfirmSeq),
-                predicateOptional(qConfirm.challenge::eq,challenge),
+                predicateOptional(qConfirm.seq::lt, prevLastConfirmSeq),
+                predicateOptional(qConfirm.challenge::eq, challenge),
+                qConfirm.state.notIn(ConfirmStateType.DELETED, ConfirmStateType.BLOCKED)
         };
 
         List<Confirm> confirmList = from(qConfirm)
@@ -70,22 +68,23 @@ public class ConfirmRepositoryImpl extends QuerydslRepositorySupport implements 
     }
 
     @Override
-    public List<Confirm> findByUserByOrderDesc(Long userSeq,Long prevLastConfirmSeq, Integer pageSize){
-        List<User> user=from(qUser)
+    public List<Confirm> findByUserByOrderDesc(Long userSeq, Long prevLastConfirmSeq, Integer pageSize) {
+        List<User> user = from(qUser)
                 .where(qUser.seq.eq(userSeq))
                 .fetch();
 
-        List<Challenge> challengeList=from(qChallenge)
+        List<Challenge> challengeList = from(qChallenge)
                 .fetch();
 
         Predicate[] predicates = new Predicate[]{
-                predicateOptional(qConfirm.seq::lt,prevLastConfirmSeq),
+                predicateOptional(qConfirm.seq::lt, prevLastConfirmSeq),
+                qConfirm.state.notIn(ConfirmStateType.DELETED, ConfirmStateType.BLOCKED)
         };
 
         List<Confirm> confirmList = from(qConfirm)
-                .innerJoin(qConfirm.challenge,qChallenge)
-                .innerJoin(qConfirm.user,qUser)
-                .where(qConfirm.user.in(user),qConfirm.challenge.in(challengeList))
+                .innerJoin(qConfirm.challenge, qChallenge)
+                .innerJoin(qConfirm.user, qUser)
+                .where(qConfirm.user.in(user), qConfirm.challenge.in(challengeList))
                 .where(predicates)
                 .orderBy(qConfirm.createdAt.desc())
                 .limit(pageSize)
@@ -96,7 +95,7 @@ public class ConfirmRepositoryImpl extends QuerydslRepositorySupport implements 
     }
 
     @Override
-    public Confirm findBySeq(Long seq){
+    public Confirm findBySeq(Long seq) {
 
         Confirm confirm = from(qConfirm)
                 .where(qConfirm.seq.in(seq))
@@ -107,10 +106,10 @@ public class ConfirmRepositoryImpl extends QuerydslRepositorySupport implements 
 
     @Transactional
     @Override
-    public void updateStateSupportedCnt(Confirm confirm, ConfirmStateType state, Integer cnt){
+    public void updateStateSupportedCnt(Confirm confirm, ConfirmStateType state, Integer cnt) {
         update(qConfirm)
-                .set(qConfirm.state,state)
-                .set(qConfirm.reportedCnt,cnt)
+                .set(qConfirm.state, state)
+                .set(qConfirm.reportedCnt, cnt)
                 .where(qConfirm.eq(confirm))
                 .execute();
 
@@ -118,10 +117,11 @@ public class ConfirmRepositoryImpl extends QuerydslRepositorySupport implements 
     }
 
     @Override
-    public Optional<Confirm> findByChallenge(Challenge challenge){
+    public Optional<Confirm> findByChallenge(Challenge challenge) {
         return from(qConfirm)
                 .select(qConfirm)
                 .where(qConfirm.challenge.eq(challenge))
+                .where(qConfirm.state.notIn(ConfirmStateType.DELETED, ConfirmStateType.BLOCKED))
                 .orderBy(qConfirm.createdAt.asc())
                 .fetch()
                 .stream()
@@ -130,12 +130,13 @@ public class ConfirmRepositoryImpl extends QuerydslRepositorySupport implements 
     }
 
     @Override
-    public List<Confirm> findByCreatedAt(LocalDateTime localDateTime){
+    public List<Confirm> findByCreatedAt(LocalDateTime localDateTime) {
 
         return from(qConfirm)
                 .innerJoin(qConfirm.challenge, qChallenge).fetchJoin()
                 .innerJoin(qConfirm.user, qUser).fetchJoin()
                 .where(qConfirm.createdAt.goe(localDateTime))
+                .where(qConfirm.state.notIn(ConfirmStateType.DELETED, ConfirmStateType.BLOCKED))
                 .fetch();
     }
 
@@ -145,7 +146,8 @@ public class ConfirmRepositoryImpl extends QuerydslRepositorySupport implements 
                 qConfirm.createdAt.goe(startDateTime),
                 qConfirm.createdAt.lt(endDateTime),
                 qConfirm.user.eq(user),
-                qConfirm.challenge.eq(challenge)
+                qConfirm.challenge.eq(challenge),
+                qConfirm.state.notIn(ConfirmStateType.DELETED, ConfirmStateType.BLOCKED)
         };
 
         return from(qConfirm)
@@ -154,15 +156,17 @@ public class ConfirmRepositoryImpl extends QuerydslRepositorySupport implements 
                 .where(predicates)
                 .fetch();
     }
+
     @Override
-    public List<Confirm> findByPeriodAndPaging(LocalDateTime startDateTime,Long prevLastConfirmSeq, Integer pageSize){
+    public List<Confirm> findByPeriodAndPaging(LocalDateTime startDateTime, Long prevLastConfirmSeq, Integer pageSize) {
         Predicate[] predicates = new Predicate[]{
                 qConfirm.createdAt.goe(startDateTime),
-                predicateOptional(qConfirm.seq::lt,prevLastConfirmSeq)
+                predicateOptional(qConfirm.seq::lt, prevLastConfirmSeq),
+                qConfirm.state.notIn(ConfirmStateType.DELETED, ConfirmStateType.BLOCKED)
         };
 
         return from(qConfirm)
-                .leftJoin(qConfirm.challenge,qChallenge).fetchJoin()
+                .leftJoin(qConfirm.challenge, qChallenge).fetchJoin()
                 .where(predicates)
                 .orderBy(qConfirm.seq.desc())
                 .limit(pageSize)
