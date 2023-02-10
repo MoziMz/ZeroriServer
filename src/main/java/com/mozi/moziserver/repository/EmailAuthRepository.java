eef778667a841a3931034f7d26a8f4406a84849d
JoHyoju04 <whgywn369@gmail.com>
2023-02-10 17:33:35 +0900

    [fix] #73 - Fix the parameter to user, not id, considering the same id in EmailAuth.

--- src/main/java/com/mozi/moziserver/repository/EmailAuthRepository.java
+++ src/main/java/com/mozi/moziserver/repository/EmailAuthRepository.java
@@ -1,6 +1,7 @@
 package com.mozi.moziserver.repository;
 
 import com.mozi.moziserver.model.entity.EmailAuth;
+import com.mozi.moziserver.model.entity.User;
 import com.mozi.moziserver.model.mappedenum.EmailAuthType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,6 +14,6 @@ import java.util.Optional;
 public interface EmailAuthRepository extends JpaRepository<EmailAuth, String> {
     Optional<EmailAuth> findByToken(String token);
 
-    @Query(value = "SELECT * FROM email_auth WHERE id = :id And type = :type ORDER BY created_at desc limit 1", nativeQuery = true)
-    Optional<EmailAuth> findByIdAndTypeOrderByCreatedAt(@Param("id")String id,@Param("type") int type);
+    @Query(value = "SELECT * FROM email_auth WHERE user_seq = :user And type = :type ORDER BY created_at desc limit 1", nativeQuery = true)
+    Optional<EmailAuth> findByUserAndTypeOrderByCreatedAt(@Param("user") User user, @Param("type") int type);
 }
