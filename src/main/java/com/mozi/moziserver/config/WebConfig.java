3adf5320d2b4d158c8cdc3b6275852ab4539bfdd
YunYeoKyeong99 <ejmacdayori2e@gmail.com>
2023-02-13 12:54:42 +0900

    [fix] Fix CORS configuration

--- src/main/java/com/mozi/moziserver/config/WebConfig.java
+++ src/main/java/com/mozi/moziserver/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.mozi.moziserver.config;
 
-import com.mozi.moziserver.common.Constant;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -10,13 +9,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     @Value("${ADMIN_ORIGIN_URL}")
-    private String origin;
+    private String adminOrigin;
+
+    @Value("${SWAGGER_ORIGIN_URL}")
+    private String swaggerOrigin;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins(origin)
+                .allowedOrigins(adminOrigin, swaggerOrigin)
                 .allowedMethods("*")
+                .allowedHeaders("*")
                 .allowCredentials(true);
     }
 }
