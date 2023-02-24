0bf2377ab00367785c58ab52d1f834d2f3e7709b
JoHyoju04 <whgywn369@gmail.com>
2023-02-24 20:07:33 +0900

    [feat] #106 - Add Admin API related modification DetailIsland

--- src/main/java/com/mozi/moziserver/adminController/AdminIslandController.java
+++ src/main/java/com/mozi/moziserver/adminController/AdminIslandController.java
@@ -1,31 +1,37 @@
 package com.mozi.moziserver.adminController;
 
 import com.mozi.moziserver.adminService.AdminIslandService;
-import com.mozi.moziserver.common.Constant;
-import com.mozi.moziserver.httpException.ResponseError;
-//import com.mozi.moziserver.model.adminRes.AdminResIsland;
-import com.mozi.moziserver.model.entity.Island;
-//import com.mozi.moziserver.model.entity.IslandImg;
-import com.mozi.moziserver.security.SessionUser;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import java.util.List;
 
 @Slf4j
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class AdminIslandController {
-//
-//    private final AdminIslandService adminIslandService;
-//
+
+    private final AdminIslandService adminIslandService;
+
+    @ApiOperation("detail 섬 이미지 수정")
+    @PutMapping(value = "/admin/detail-islands/{seq}")
+    public ResponseEntity<Object> updateIslandImg(
+            @PathVariable("seq") Long seq,
+            @RequestPart(value = "img", required = false) MultipartFile islandImg,
+            @RequestPart(value = "islandThumbnailImg", required = false) MultipartFile islandThumbnailImg
+    ) {
+
+        if (islandImg != null || islandThumbnailImg != null) {
+            adminIslandService.updateImgOfDetailIsland(seq, islandImg, islandThumbnailImg);
+        }
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 //    @ApiOperation("섬 리스트 조회")
 //    @GetMapping(value = "/admin/islands")
 //    public List<Island> getIsland(
@@ -84,7 +90,6 @@ public class AdminIslandController {
 //
 //        return new ResponseEntity<>(HttpStatus.OK);
 //    }
-//
 //    @ApiOperation("섬 이미지 수정")
 //    @PutMapping(value = "/admin/islands/{type}/island-imgs/{level}")
 //    public ResponseEntity<Object> updateIslandImg(
@@ -100,7 +105,6 @@ public class AdminIslandController {
 //
 //        return new ResponseEntity<>(HttpStatus.OK);
 //    }
-//
 //    @ApiOperation("섬 삭제")
 //    @DeleteMapping("/admin/islands/{type}")
 //    public ResponseEntity<Object> deleteIsland(
