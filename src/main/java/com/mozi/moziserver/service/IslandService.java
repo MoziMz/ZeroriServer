d2f45444e6fe3f91c050288f62645e126b7b3573
JoHyoju04 <whgywn369@gmail.com>
2023-07-25 20:58:58 +0900

    [feat] #139 - Add method related to DetailIsland for joining and modify firstCreateUserIsland method name

--- src/main/java/com/mozi/moziserver/service/IslandService.java
+++ src/main/java/com/mozi/moziserver/service/IslandService.java
@@ -14,7 +14,6 @@ import com.mozi.moziserver.repository.UserIslandRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -23,11 +22,11 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class IslandService {
-    
+
     private final UserRewardService userRewardService;
     private final PostboxMessageAnimalService postboxMessageAnimalService;
     private final AnimalService animalService;
-    
+
     private final IslandRepository islandRepository;
     private final UserIslandRepository userIslandRepository;
     private final DetailIslandRepository detailIslandRepository;
@@ -38,7 +37,7 @@ public class IslandService {
         return islandRepository.findById(seq)
                 .orElseThrow(ResponseError.NotFound.ISLAND_NOT_EXISTS::getResponseException);
     }
-    
+
     public List<Island> getIslandListOrderBySeq() {
 
         return islandRepository.findAllByOrderBySeqAsc();
@@ -52,8 +51,13 @@ public class IslandService {
     }
 
     public DetailIsland getFirstDetailIslandOfIsland(Long islandSeq) {
-
         return detailIslandRepository.findByIslandSeqAndAnimalTurnAndItemTurn(islandSeq, 0, 0)
+                .orElseThrow(ResponseError.NotFound.DETAIL_ISLAND_NOT_EXISTS::getResponseException);
+    }
+
+    public DetailIsland getDetailIslandForJoin() {
+        //회원가입하자마자 튜토리얼을 고려
+        return detailIslandRepository.findByIslandSeqAndAnimalTurnAndItemTurn(1L, 1, 1)
                 .orElseThrow(ResponseError.NotFound.DETAIL_ISLAND_NOT_EXISTS::getResponseException);
     }
 
@@ -85,15 +89,10 @@ public class IslandService {
 
         return userIslandRepository.findTopByUserOrderByIsland(user);
     }
-    
+
     public List<UserIsland> getUserIslandListOrderByIslandSeq(User user) {
 
         return userIslandRepository.findAllByUserOrderByIsland(user);
-    }
-
-    public void firstCreateUserIsland(User user) {
-
-        createUserIsland(user, 1L);
     }
 
     public void createUserIsland(User user, Long islandSeq) {
@@ -105,7 +104,21 @@ public class IslandService {
                 .build();
         userIslandRepository.save(userIsland);
 
-        postboxMessageAnimalService.createFirstMessageInIsland(user, islandSeq);
+        postboxMessageAnimalService.createFirstPostboxMessageAnimal(user, islandSeq);
+
+    }
+
+    @Transactional
+    public void createUserIslandForJoin(User user) {
+
+        DetailIsland detailIsland = getDetailIslandForJoin();
+        UserIsland userIsland = UserIsland.builder()
+                .detailIsland(detailIsland)
+                .user(user)
+                .build();
+        userIslandRepository.save(userIsland);
+
+        postboxMessageAnimalService.createPostboxMessageAnimalForJoin(user, 1L);
     }
 
     @Transactional
